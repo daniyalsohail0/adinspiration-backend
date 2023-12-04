18e9f5371098f879e40f2c94d0c6cde56f8a47de
Daniyal Sohail <daniyalsohail0@gmail.com>
2023-12-04 23:04:34 +0000

    all completed

--- src/controller/savedCollectionsController.ts
+++ src/controller/savedCollectionsController.ts
@@ -1,43 +1,55 @@
 import { Request, Response } from "express";
 import User from "../model/userModel";
-import { resolveSoa } from "dns";
 
-export const newSavedCollection = async (req: Request, res: Response) => {
+export const addSavedCollection = async (req: Request, res: Response) => {
   try {
     const userId = req.params.userId;
-    const {collectionId} = req.body;
-    
-    const user = await User.findById(userId).populate("savedCollections")
+    const { collectionId } = req.body;
 
-    if(!user) {
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
       return res.status(404).json({
         success: false,
-        message: "User not found."
-      })
+        message: "User not found.",
+      });
     }
 
-    if(user.savedCollections.includes(collectionId)) {
+    if (user.savedCollections.includes(collectionId)) {
       return res.status(401).json({
         success: false,
-        message: "This collection is already saved."
-      })
+        message: "This collection is already saved.",
+      });
     }
 
-    if(user.savedCollections.length >= 20 && user.subscription !== "Pro") {
+    if (user.savedCollections.length >= 20 && user.subscription !== "Pro") {
       return res.status(401).json({
         success: false,
-        message: "Please upgrade to Pro user. You are only allowed 20 saved items."
-      })
+        message:
+          "Please upgrade to Pro user. You are only allowed 20 saved items.",
+      });
     }
 
     user.savedCollections.push(collectionId);
 
-    await user.save()
+    await user.save();
+
+    const updatedCollection = await User.findById(userId)
+      .select("savedCollections")
+      .populate("savedCollections").exec();
+
+    
+    if(!updatedCollection) {
+      return res.status(401).json({
+        success: false,
+        message: "Try again."
+      })
+    }
 
     res.status(201).json({
       success: true,
-      message: user.savedCollections,
-    })
+      message: updatedCollection.savedCollections,
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -46,10 +58,61 @@ export const newSavedCollection = async (req: Request, res: Response) => {
   }
 };
 
-export const removeSavedCollection = async () => {
-  console.log("saved item removed");
+export const removeSavedCollection = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.userId;
+    const { collectionId } = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $pull: { savedCollections: collectionId } },
+      { new: true }
+    ).populate("savedCollections");
+
+    if (updatedUser) {
+      res.status(200).json({
+        success: true,
+        data: updatedUser.savedCollections,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "User not found with the specified ID.",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
 };
 
-export const getSavedCollections = async () => {
-  console.log("all saved collections");
+export const getSavedCollections = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.userId;
+
+    const user = await User.findById(userId)
+      .populate("savedCollections")
+      .select("savedCollections");
+
+    console.log(user);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found.",
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
 };
--- src/routes/savedCollectionRoutes.ts
+++ src/routes/savedCollectionRoutes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { authVerify } from "../middleware/authVerify";
-import { newSavedCollection } from "../controller/savedCollectionsController";
+import { addSavedCollection, getSavedCollections, removeSavedCollection } from "../controller/savedCollectionsController";
 
 const router = express.Router();
 
-router.post("/:userId/saved", authVerify, newSavedCollection);
+router.post("/:userId/saved", authVerify, addSavedCollection);
+
+router.put("/:userId/saved", authVerify, removeSavedCollection);
+
+router.get("/:userId/saved", authVerify, getSavedCollections);
 
 export default router;
